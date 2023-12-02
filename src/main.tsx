98dd9705fc12c17df997e4f3e4444e4c01a926b4
dimitar <dimitar.daskalov22@gmail.com>
2023-12-02 18:10:49 +0200

    feat: test google analytics

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,17 @@ import { router } from "./helpers/router";
 import ToastProvider from "./components/providers/ToastProvider";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./components/providers/i18n";
+import ReactGA from "react-ga4";
+
+const gaKey = import.meta.env.VITE_GOOGLE_ANALYTICS_KEY;
+
+ReactGA.initialize(gaKey);
+
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+  title: `Visited ${window.location.pathname}`,
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
